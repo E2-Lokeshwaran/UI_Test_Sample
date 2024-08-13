fe90496bd415d4c7c8784e0ae12b29489b01587e
E2-Lokeshwaran <lokeshwaran.s@e2infosystem.com>
2024-08-13 12:27:36 +0530

    Initial Commit

---
+++ UI_Test_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_Test_Sample
+//
+//  Created by Lokeshwaran on 13/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
